d72b8b7010351f62aa1bc507d67c4788584dfc2f
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-05-19 21:55:22 +0200

    add clerk provider, react hot toast

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -42,6 +42,7 @@ SheetPortal.displayName = SheetPrimitive.Portal.displayName;
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
 >(({ className, children, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider>
+      <Toaster position="bottom-right" reverseOrder={false} />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
