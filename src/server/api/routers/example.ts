f0482132e4a8fa50cfedccd8759c8eaafe4be2f8
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-05-19 21:22:20 +0200

    initialize project

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(async ({ input }) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+});
