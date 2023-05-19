21c1a96be9f6f0172e5558c0abd40202afbc5a40
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-05-19 21:27:20 +0200

    add drizzle orm

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { env } from "~/env.mjs";
+
+const connectionString = () => {
+  if (env.NODE_ENV === "production") {
+    return env.POSTGRES_URL + "?sslmode=require";
+  }
+
+  return env.POSTGRES_URL;
+};
+
+const pool = new Pool({
+  connectionString: connectionString(),
+});
+
+export const db = drizzle(pool);
--- src/env.mjs
+++ src/env.mjs
@@ -1,5 +1,5 @@
-import { z } from "zod";
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   /**
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    POSTGRES_URL: z.string().url(),
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    POSTGRES_URL: process.env.POSTGRES_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
 });
