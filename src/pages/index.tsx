5399535334132e5b55200d60738918483c33ade7
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-05-20 11:13:37 +0200

    update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
-export default function HomePage() {
-  return <div>Index page</div>;
+import Head from "next/head";
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Home" />
+      </Head>
+      <div className="flex h-screen items-center justify-center">
+        <Button asChild>
+          <Link href="/dashboard">Go to the dashboard</Link>
+        </Button>
+      </div>
+    </>
+  );
 }
